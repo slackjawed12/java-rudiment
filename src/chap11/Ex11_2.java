fa2f47692de8df65871c3160686867ef41c76907
narustudio <slackjawed12@gmail.com>
2023-02-28 11:43:29 +0900

    ArrayList, Stack, Queue

---
+++ src/chap11/Ex11_2.java
@@ -0,0 +1,32 @@
+package chap11;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Ex11_2 {
+    public static void main(String[] args) {
+        Stack<String> st = new Stack<>();
+        Queue<String> q = new LinkedList<>();
+
+        // Stack Class
+        st.push("0");
+        st.push("1");
+        st.push("2");
+
+        // Queue interface - LinkedList 구현
+        q.offer("0");
+        q.offer("1");
+        q.offer("2");
+
+        System.out.println("= Stack =");
+        while(!st.empty()){
+            System.out.println(st.pop());
+        }
+
+        System.out.println("= Queue =");
+        while(!q.isEmpty()){
+            System.out.println(q.poll());
+        }
+    }
+}
